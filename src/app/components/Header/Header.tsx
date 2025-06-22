f61dc63db4eec4d2c573987eb9b7b0786ea9f8b5
clappingmin <clappingmin@gmail.com>
2025-06-22 22:32:09 +0900

    style: 헤더 기본 스타일 추가

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Header.module.scss';
+import PrimaryButton from '../buttons/PrimaryButton/PrimaryButton';
+import GrayButton from '../buttons/GrayButton/GrayButton';
+import WhiteButton from '../buttons/WhiteButton/WhiteButton';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.topContainer}>
+        <span className={styles.title}>
+          옛날<strong>게임</strong>나라
+        </span>
+        <input className={styles.searchbar}></input>
+        <div className={styles.buttons}>
+          <PrimaryButton>Log in</PrimaryButton>
+          <GrayButton>Sign up</GrayButton>
+        </div>
+      </div>
+      <div className={styles.bottomContainer}>
+        <WhiteButton>슈게임</WhiteButton>
+        <WhiteButton>로이월드</WhiteButton>
+        <WhiteButton>쥬쥬게임</WhiteButton>
+        <WhiteButton>비비빅</WhiteButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
