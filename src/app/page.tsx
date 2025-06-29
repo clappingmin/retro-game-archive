11ea3766d7944cef779318e0bfb21d951e9ff36b
clappingmin <clappingmin@gmail.com>
2025-06-29 23:12:58 +0900

    styel: game list style 전역으로 빼기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,20 @@ export default async function Home() {
   const recommendedGames = await api.getRecommendedGames();
   const recetGames = await api.getRecentGames();
 
-  console.log(recetGames);
-
   return (
     <div>
       {/* 최근 게임 */}
-      <div></div>
+      <div>최근 게임</div>
+      <div className="list">
+        {recetGames.map((game) => (
+          <GameCard game={game} key={`recent-game-${game.id}`} />
+        ))}
+      </div>
       {/* 추천 게임 */}
       <div>추천 게임</div>
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,1fr))] gap-4">
         {recommendedGames.map((game) => (
-          <GameCard game={game} />
-        ))}
-        {recommendedGames.map((game) => (
-          <GameCard game={game} />
-        ))}
-        {recommendedGames.map((game) => (
-          <GameCard game={game} />
-        ))}
-        {recommendedGames.map((game) => (
-          <GameCard game={game} />
+          <GameCard game={game} key={`recommended-game-${game.id}`} />
         ))}
       </div>
     </div>
